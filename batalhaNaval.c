ffb740bddbbb6c0f57695e55bc03b065067cb232
AlanISX <alanmagalhaes980@gmail.com>
2025-04-01 15:49:02 +0000

    Melhora do código

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,55 +2,61 @@
 
 int main()
 {
-    // Array para localização do tabuleiro
+    // Letras das linhas do tabuleiro
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    // Números das colunas do tabuleiro
     int coluna[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    // Declarando matriz
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    // Matriz 10x10 para o estado do tabuleiro
+    int tabuleiro[10][10];
 
-    for (int i = 4; i < 7; i++)
-    {
-
-        tabuleiro[4][i] = 3;
-    }
-
-    for (int i = 6; i < 9; i++)
-    {
-
-        tabuleiro[i][2] = 3;
-    }
-
-    // Exibindo tabuleiro
-    printf("-=- TABULEIRO BATALHA NAVAL -=-\n");
-    printf("\n");
-    printf("   ");
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%c ", linha[i]);
-    }
-    printf("\n");
-
-    for (int i = 0; i < 10; i++)
-    {
-        printf(" ");
-        printf("%d", coluna[i]);
-        for (int j = 0; j < 10; j++)
-        {
-            printf(" %d", tabuleiro[i][j]);
+    // Inicializando o tabuleiro com 0 (água)
+    for (int i = 0; i < 10; i++) {  
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+            printf("%d ", tabuleiro[i][j]); // Imprime o valor inicial
         }
-        printf("\n");
+        printf("\n"); // Nova linha após cada linha do tabuleiro
     }
 
-    return 0;
+    // Preenchendo navios no tabuleiro
+    for (int i = 6; i < 9; i++) {
+        tabuleiro[4][i] = 3; // Navio na linha 4, colunas 6 a 8
+    }
+
+    for (int i = 6; i < 9; i++) {
+        tabuleiro[i][2] = 3; // Navio na coluna 2, linhas 6 a 8
+    }
+
+    for (int i = 3; i < 6; i++) {
+        tabuleiro[i][i] = 3; // Navio na diagonal primária
+    }
+
+    // Diagonal secundária: soma dos índices igual a 9
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (i + j == 9) {
+                tabuleiro[i][j] = 3; // Navio na diagonal secundária
+            }
+        }
+    }
+
+    // Exibindo o tabuleiro
+    printf("-=- TABULEIRO BATALHA NAVAL -=-\n\n");
+
+    printf("   "); // Alinhamento das colunas
+    for (int i = 0; i < 10; i++) {
+        printf("%c ", linha[i]); // Letras das colunas
+    }
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf(" %d", coluna[i]); // Números das linhas
+        for (int j = 0; j < 10; j++) {
+            printf(" %d", tabuleiro[i][j]); // Valores da célula
+        }
+        printf("\n"); // Nova linha após cada linha do tabuleiro
+    }
+
+    return 0; 
 }
